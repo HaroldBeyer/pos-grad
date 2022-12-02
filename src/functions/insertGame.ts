8dcea45dd0bdf7874662a6aaae21b734da60ae9f
Haroldo <haroldobeyerbacellar@gmail.com>
2022-12-01 22:05:31 -0300

    Fix error

--- src/functions/insertGame.ts
+++ src/functions/insertGame.ts
@@ -1,19 +1,18 @@
 import { Context, APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda'
-import { GameRatingModel } from '../models/gameRating.model';
-import { GameRating } from '../entities/gameRating'
 import { Game } from '../entities/game';
+import { GameModel } from '../models/game.model';
 
 export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     const body = JSON.parse(event.body ?? '')
     console.log('body: ', body)
     const {genre, name} = body 
-    const gameRating = {
+    const game = {
         genre,
         name
     } as Game
 
-    const result = await GameRatingModel.create({
-        ...gameRating
+    const result = await GameModel.create({
+        ...game
     })
 
     return {
